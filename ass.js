7436790ab89f895e10f4eea71494a6001f303f2c
Anden Wieseler <ttechno@icloud.com>
2021-07-22 00:06:00 -0500

    Create ass.js

---
+++ ass.js
@@ -0,0 +1,9 @@
+module.exports = {
+	name: 'ass',
+	execute(message, args) {
+    client.channels.cache.get('867631196527919104').send('<@615882904848826389> you\'re such a good boy. ')
+    for (var x = 0; x <= 40; x++){
+      client.channels.cache.get('867631196527919104').send('<@615882904848826389> <:cum:784134896477405264>')
+    }
+	},
+};
