f8e8d6dfee2e6f0d656929b0c1d4e58a0414ba32
Anden Wieseler <ttechno@icloud.com>
2021-07-22 20:02:15 -0500

    Update ass.js

--- ass.js
+++ ass.js
@@ -4,9 +4,10 @@ const client = new Discord.Client();
 module.exports = {
 	name: 'ass',
 	execute(message, args) {
-		client.channels.cache.get('867631196527919104').send('<@615882904848826389> you\'re such a good boy. ')
+		var luke = client.channels.cache.get('867631196527919104')
+		luke.send('<@615882904848826389> you\'re such a good boy. ')
 		for (var x = 0; x <= 40; x++){
-			client.channels.cache.get('867631196527919104').send('<@615882904848826389> <:cum:784134896477405264>')
+			luke.send('<@615882904848826389> <:cum:784134896477405264>')
 		}
 	},
 };
