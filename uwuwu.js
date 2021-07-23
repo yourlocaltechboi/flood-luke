d3998a0d6b4eb027b77b3de16bb620e962122cbd
Anden Wieseler <ttechno@icloud.com>
2021-07-22 20:13:41 -0500

    Update uwuwu.js

--- uwuwu.js
+++ uwuwu.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 module.exports = {
 	name: 'ass',
 	execute(message, args) {
-		var luke = client.channels.cache.find(x => x.id == "352496750327496725")
+		var luke = client.channels.cache.find(x => x.id == "867631196527919104")
 		luke.send('<@615882904848826389> you\'re such a good boy. ')
 		for (var x = 0; x <= 40; x++){
 			luke.send('<@615882904848826389> <:cum:784134896477405264>')
