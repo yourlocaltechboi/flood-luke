f5d9b296c9911bbf9261bc26d6081e5c4f0d8871
Anden Wieseler <ttechno@icloud.com>
2021-07-22 21:05:36 -0500

    Update oo.js

--- oo.js
+++ oo.js
@@ -5,9 +5,9 @@ module.exports = {
 	name: 'ass',
 	execute(message, args) {
 		const luke = client.channels.cache.fetch("867631196527919104")
-		await luke.send('<@615882904848826389> you\'re such a good boy. ')
+		luke.send('<@615882904848826389> you\'re such a good boy. ')
 		for (var x = 0; x <= 40; x++){
-			await luke.send('<@615882904848826389> <:cum:784134896477405264>')
+			luke.send('<@615882904848826389> <:cum:784134896477405264>')
 		}
 	},
 };
