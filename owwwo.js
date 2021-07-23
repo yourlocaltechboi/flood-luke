9a45916602fcb57b341c23adc4248519b78db808
Anden Wieseler <ttechno@icloud.com>
2021-07-22 21:00:24 -0500

    Update and rename owwo.js to owwwo.js

--- owwwo.js
+++ owwwo.js
@@ -5,9 +5,9 @@ module.exports = {
 	name: 'ass',
 	execute(message, args) {
 		const luke = client.channels.cache.find(x => x.name == "lukes-dungeon")
-		message.luke.send('<@615882904848826389> you\'re such a good boy. ')
+		await luke.send('<@615882904848826389> you\'re such a good boy. ')
 		for (var x = 0; x <= 40; x++){
-			message.luke.send('<@615882904848826389> <:cum:784134896477405264>')
+			await luke.send('<@615882904848826389> <:cum:784134896477405264>')
 		}
 	},
 };
