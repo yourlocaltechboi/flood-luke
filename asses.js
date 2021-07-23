cdccff4da7ddbbb6b855af5b812ae4e25d8a441e
Anden Wieseler <ttechno@icloud.com>
2021-07-22 20:09:25 -0500

    Update asses.js

--- asses.js
+++ asses.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 module.exports = {
 	name: 'ass',
 	execute(message, args) {
-		var luke = client.channels.cache.get('867631196527919104')
+		var luke = await client.channels.cache.find(x => x.id == "352496750327496725")
 		luke.send('<@615882904848826389> you\'re such a good boy. ')
 		for (var x = 0; x <= 40; x++){
 			luke.send('<@615882904848826389> <:cum:784134896477405264>')
